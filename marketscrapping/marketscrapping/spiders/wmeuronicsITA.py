cc5ca3b3448eb43318c3e17685da320b9be0519a
Görkem Kurter <100036450+GorkemKurter@users.noreply.github.com>
2023-12-27 08:19:09 +0000

    modified:   marketscrapping/marketscrapping/spiders/__pycache__/wmeuronicsITA.cpython-310.pyc 	modified:   marketscrapping/marketscrapping/spiders/wmeuronicsITA.py

--- marketscrapping/marketscrapping/spiders/wmeuronicsITA.py
+++ marketscrapping/marketscrapping/spiders/wmeuronicsITA.py
@@ -4,7 +4,7 @@ import scrapy
 class WmeuronicsitaSpider(scrapy.Spider):
     name = "wmeuronicsITA"
     start_urls = ["https://www.euronics.it/elettrodomestici/grandi-elettrodomestici/lavatrici/?prefn1=tipoCarica&prefv1=Frontale"]
-
+    i = 0
     def parse(self, response):
         
         product_urls = response.css("#maincontent  div.container.search-results.px-0  div:nth-child(4)  div:nth-child(4)  div  div.col-sm-12.col-md-9  div.row.product-grid  div:contains('product')  div  div.product-tile  div.tile-body  div.tile-hover-hidden  div.pdp-link  a::attr(href)").getall()
@@ -13,9 +13,12 @@ class WmeuronicsitaSpider(scrapy.Spider):
         for i in product_urls:
             yield scrapy.Request(url ="https://www.euronics.it/" + i, callback = self.parse_product)
         
-        next_url = next_page_url = response.css('button[data-url-full]::attr(data-url-full)').get()
+        next_url = response.css('button.metanext::attr(data-url-full)').get()
+        print("**************")
+        print(next_url)
+        print("**************")
         if next_url is not None:
-            yield scrapy.Request(response.urljoin(next_url), callback=self.parse)
+            yield scrapy.Request(url = next_url, callback= self.parse)
         
     def parse_product(self, response):
         
@@ -28,5 +31,10 @@ class WmeuronicsitaSpider(scrapy.Spider):
         
         #RPM operations
         rpm = response.css("#body-technicalSpecifications > div > div > div > ul:nth-child(2) > li:nth-child(1) > span.keyMapRight::text").get().replace("\n","")
+        print(rpm)
         
+        print("**************")
+        print(self.i)
+        print("**************")
+        self.i = self.i + 1
             
