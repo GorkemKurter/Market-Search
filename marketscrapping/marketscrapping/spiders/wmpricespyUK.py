54947c74546e5681d3ff8b759a119b11823d9fe6
Görkem Kurter <100036450+GorkemKurter@users.noreply.github.com>
2024-01-10 23:42:02 +0300

    Update wmpricespyUK.py

--- marketscrapping/marketscrapping/spiders/wmpricespyUK.py
+++ marketscrapping/marketscrapping/spiders/wmpricespyUK.py
@@ -1,4 +1,9 @@
 import scrapy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
 class WmpricespyukSpider(scrapy.Spider):
@@ -6,24 +11,27 @@ class WmpricespyukSpider(scrapy.Spider):
     start_urls = ["https://pricespy.co.uk/c/washing-machines?1247=2342"]
 
     def parse(self, response):
-        
-        product_links = response.css("div  section  div.Content-sc-2fu3f8-2.bugHkt  div  div  div article  a::attr(href)").getall()
-        '''for i in range(len(product_links)):
-            if i == "/":
-                product_links.pop(i)'''
-        print(product_links)
-        print("******************************")
-        for i in product_links:
-            yield scrapy.Request(url=("https://pricespy.co.uk/" + i + "#properties") ,callback = self.parse_product)
 
-#\#properties > div > section > section > div > div > div.hideInViewports-sc-0-0.iwivxM > div > section > div:nth-child(2) > div:nth-child(2) > span
-        
+        product_links = response.css("div  section  div.Content-sc-2fu3f8-2.bugHkt  div  div  div article  a::attr(href)").getall()
+        for i in product_links:
+            yield scrapy.Request(url=("https://pricespy.co.uk" + i + "#properties") ,callback = self.parse_product,meta={'product_link': i})
     def parse_product(self, response):
-        
+
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
+        url = f"https://pricespy.co.uk{response.meta.get('product_link', '')}#properties"
+        driver.get(url)
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR,'#\#properties > div > section > section > div > div > div.hideInViewports-sc-0-0.iwivxM > div > section > div:nth-child(2) > div:nth-child(2) > span')))
+
+            product_name = element.text
+            print(product_name)
+            print("xxxxxxxxxxxxxxxxxxxxxxx")
+
+        finally:
+            driver.quit()
+
         brand_name =  response.css('div  section  section  div div section span  a::text').get()
-        model_name = response.css("div  section  section  div  div.hideInViewports-sc-0-0.iwivxM").get()      
-        #model_name = response.css("div div section div div.SectionWrapper-sc-ia0zhw-0.bqrwQK section div.StyledList--1mpttw7.czBGBu.StyledPanel--lryowj.llXyJQ div div:nth-child(2) div.Column-sc-0-2.kMCFmk span.Text--o69vef.gLKIHP.bodysmalltext.PropertyValue-sc-0-6.guNUSr::text").getall()
-#\#properties > div > section > section > div > div > div.hideInViewports-sc-0-0.iwivxM > div > section > div:nth-child(2) > div:nth-child(2) > span        
         print(brand_name)
-        print(model_name)
-        
